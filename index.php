fa1121b985fa53a392874e03bc41dcced219c4a0
ThanhTung-58TH2 <45089231+ThanhTung-58TH2@users.noreply.github.com>
2018-12-06 11:22:00 +0700

    trung tam

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.8">
+        <meta http-euiv="X-UA-Compatible" content="edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        $biengido='images/banana.jpeg';
+        ?>
+        <img src="<?php echo $biengido; ?>" alt="hello">
+
+    </body>
+</html>
